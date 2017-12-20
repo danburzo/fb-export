123353e52a0c490492e3808421bb1e715a0e0af2
Dan Burzo <danburzo@gmail.com>
2017-12-20 11:53:36 +0200

    First tool: fetch posts

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # fb-export
+
 Export (most) of your Facebook data using Node.js and the Graph API.
+
+## Introduction
+
+## Installation
+
+You'll need Node.js, NPM and, optionally, Yarn (which these instructions use throughout). Since I wrote the scripts with ES6 syntax for its conciseness, this only probably works with more recent versions of Node. (I'm currently using `v8.9.0`).
+
+Clone the repository to your computer and run `yarn` (or `npm install`) in your project's folder to install all the necessary dependencies.
+
+Rename the `sample.config.json` file to `config.json` and put in the *access token* you can get from the [Facebook Graph API Explorer](https://developers.facebook.com/tools/explorer/):
+
+1. Click on _Get access token_ and choose _Get User Access token_.
+2. Check all the permissions in the first set ("User Access Permissions").
+3. Put the access token in your `config.json`
+
+(Since this allows anyone to access your data through the API, we're not putting `config.json` on GitHub.)
+
+
---
+++ tools/posts.js
@@ -0,0 +1,58 @@
+let fs = require('fs-extra');
+let rp = require('request-promise');
+
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const post_fields = [
+	'id',
+	'message',
+	'created_time',
+	'link',
+	'picture',
+	'description',
+	'shares',
+	'likes',
+	'reactions',
+	'sharedposts',
+	'comments{comments,created_time,from,message}',
+	'attachments',
+	'type'
+];
+
+fetch_posts('https://graph.facebook.com/v2.11/me/posts', 'export/posts.json');
+
+/* 
+	Functions
+	------------------------------------------------------------
+*/
+
+function fetch_posts(api, outFile) {
+	let posts = [];
+	let url_queue = [api];
+	fetch_posts_page(url_queue, function(data) {
+		fs.outputFile(outFile, JSON.stringify(data, null, 2));
+	});
+};
+
+function fetch_posts_page(pages_queue, callback, accumulator = []) {
+	if (pages_queue.length) {
+		let uri = pages_queue.shift();
+		rp({
+			uri: uri,
+			qs: {
+				access_token: config.access_token,
+				limit: 100,
+				fields: post_fields.join(',')
+			},
+			json: true
+		}).then(response => {
+			fetch_posts_page(
+				pages_queue.concat(
+					response.paging && response.paging.next ? 
+						[response.paging.next] : []
+				), callback, accumulator.concat(response.data));
+		})
+	} else {
+		callback(accumulator);
+	}
+}
+
